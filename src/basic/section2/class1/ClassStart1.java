2e178e39cfb9d18bbacfbbce6d4a467b813f7ed1
jeonginh0 <stitch010913@gmail.com>
2026-05-23 19:54:02 +0900

    study: 클래스 도입 전 코드

---
+++ src/basic/section2/class1/ClassStart1.java
@@ -0,0 +1,27 @@
+package basic.section2.class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        /**
+         * 아래 코드처럼 코드를 작성하면 학생이 늘어날 때 마다 변수를 추가로 선언해야 하며,
+         * 출력하는 코드도 추가해야 한다.
+          */
+        String student1Name = "학생1";
+        int studnet1Age = 15;
+        int student1Grade = 90;
+
+        String student2Name = "학생2";
+        int studnet2Age = 16;
+        int student2Grade = 80;
+
+        String student3Name = "학생3";
+        int studnet3Age = 17;
+        int student3Grade = 60;
+
+        System.out.println("이름:" + student1Name + " 나이:" + studnet1Age + " 성적:" + student1Grade);
+        System.out.println("이름:" + student2Name + " 나이:" + studnet2Age + " 성적:" + student2Grade);
+        System.out.println("이름:" + student3Name + " 나이:" + studnet3Age + " 성적:" + student3Grade);
+    }
+
+}
---
+++ src/basic/section2/class1/ClassStart2.java
@@ -0,0 +1,23 @@
+package basic.section2.class1;
+
+public class ClassStart2 {
+
+    public static void main(String[] args) {
+        /**
+         * 배열로 ClassStart1의 코드를 리팩터링
+         * 하지만 지금처럼 이름, 나이, 성적을 따로 나누어서 관리하는 것은 사람이 관리하기 좋은 방식이 아니다.
+         * '학생'이라는 개념을 하나로 묶으면 관리하기 편해진다.
+         * 각각의 학생 별로 본인의 이름, 나이, 성적을 관리하는 것이다.
+          */
+        String[] studentNames = {"학생1", "학생2", "학생3"};
+        int[] studentAges = {15, 16, 17};
+        int[] studentGrades = {90, 80, 60};
+
+        for (int i = 0; i < studentNames.length; i++){
+            System.out.println("이름:" + studentNames[i] + " 나이:" + studentAges[i] + " 성적:" + studentGrades[i]);
+        }
+
+
+    }
+
+}
