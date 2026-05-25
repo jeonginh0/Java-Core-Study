5e274077755cfb1d71d1dd85e828929cab8e22ac
jeonginh0 <stitch010913@gmail.com>
2026-05-26 00:41:21 +0900

    study: 기본 생성자 학습

---
+++ src/basic/section/class4/MemberDefault.java
@@ -0,0 +1,9 @@
+package basic.section.class4;
+
+public class MemberDefault {
+    String name;
+
+    MemberDefault() {
+        System.out.println("생성자 호출");
+    }
+}
---
+++ src/basic/section/class4/MemberDefaultMain.java
@@ -0,0 +1,10 @@
+package basic.section.class4;
+
+public class MemberDefaultMain {
+
+    public static void main(String[] args) {
+
+        MemberDefault md = new MemberDefault();
+    }
+
+}
