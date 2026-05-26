49450c544468f72552769b5711f2e3640d9bd8e1
jeonginh0 <stitch010913@gmail.com>
2026-05-26 17:20:54 +0900

    study: static 실습

---
+++ src/basic/section/class7/ex/MathArrayUtils.java
@@ -0,0 +1,42 @@
+package basic.section.class7.ex;
+
+public class MathArrayUtils {
+
+    private MathArrayUtils() {
+    }
+
+    public static int sum(int[] values) {
+        int total = 0;
+        for (int value : values) {
+            total += value;
+        }
+        return total;
+    }
+
+    public static double average(int[] values) {
+        return (double) sum(values) / values.length;
+    }
+
+    public static int min(int[] values) {
+        int minValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] < minValue) {
+                minValue = values[i];
+            }
+        }
+
+        return minValue;
+    }
+
+    public static int max(int[] values) {
+        int maxValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] > maxValue) {
+                maxValue = values[i];
+            }
+        }
+
+        return maxValue;
+    }
+
+}
