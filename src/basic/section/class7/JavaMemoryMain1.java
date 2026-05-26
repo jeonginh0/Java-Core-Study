8e909193f8cd5b7e5a2a9e4d1d3f80aa02c669a3
jeonginh0 <stitch010913@gmail.com>
2026-05-26 16:15:43 +0900

    study: 스택과 힙 영역의 밀접한 관계 학습

---
+++ src/basic/section/class7/JavaMemoryMain1.java
@@ -0,0 +1,24 @@
+package basic.section.class7;
+
+public class JavaMemoryMain1 {
+    /*
+    스택 구조. main -> method1 -> method2 -> method2 -> method1 -> main
+     */
+    public static void main(String[] args) {
+        System.out.println("main start");
+        method1(10);
+        System.out.println("main end");
+    }
+
+    static void method1(int m1) {
+        System.out.println("method1 start");
+        int cal = m1 * 2;
+        method2(cal);
+        System.out.println("method1 end");
+    }
+
+    static void method2(int m2) {
+        System.out.println("method2 start");
+        System.out.println("method2 end");
+    }
+}
---
+++ src/basic/section/class7/JavaMemoryMain2.java
@@ -0,0 +1,23 @@
+package basic.section.class7;
+
+public class JavaMemoryMain2 {
+
+    public static void main(String[] args) {
+        System.out.println("main start");
+        method1();
+        System.out.println("main end");
+    }
+
+    static void method1() {
+        System.out.println("method1 start");
+        Data data1 = new Data(1);
+        method2(data1);
+        System.out.println("method1 end");
+    }
+
+    static void method2(Data data2) {
+        System.out.println("method2 start");
+        System.out.println("data2.value = " + data2.getValue());
+        System.out.println("method2 end");
+    }
+}
