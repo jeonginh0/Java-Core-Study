e4de23d0038553127068f9c12fb121617c072121
jeonginh0 <stitch010913@gmail.com>
2026-06-08 18:15:03 +0900

    study: 래퍼 클래스 문제 풀이 학습

---
+++ src/intermediate1/section/class4/wrapper/test/WrapperTest3.java
@@ -0,0 +1,17 @@
+package intermediate1.section.class4.wrapper.test;
+
+public class WrapperTest3 {
+
+    public static void main(String[] args) {
+        String str = "100";
+        Integer integer1 = Integer.valueOf(str);
+        System.out.println("integer1 = " + integer1);
+
+        int intValue = integer1.intValue();
+        System.out.println("intValue = " + intValue);
+
+        int integer2 = Integer.valueOf(intValue);
+        System.out.println("integer2 = " + integer2);
+    }
+
+}
---
+++ src/intermediate1/section/class4/wrapper/test/WrapperTest4.java
@@ -0,0 +1,18 @@
+package intermediate1.section.class4.wrapper.test;
+
+public class WrapperTest4 {
+
+    public static void main(String[] args) {
+        String str = "100";
+
+        Integer integer1 = Integer.valueOf(str);
+        System.out.println("integer1 = " + integer1);
+
+        int intValue = integer1;
+        System.out.println("intValue = " + intValue);
+
+        int integer2 = intValue;
+        System.out.println("integer2 = " + integer2);
+    }
+
+}
