11f62d5bdca6f7b9cb48af67fa9e48c3330a4f3d
jeonginh0 <stitch010913@gmail.com>
2026-06-15 22:37:10 +0900

    study: 제네릭 타입 예제 코드 학습

---
+++ src/intermediate2/class1/generic/test/ex3/unit/Marine.java
@@ -0,0 +1,8 @@
+package intermediate2.class1.generic.test.ex3.unit;
+
+public class Marine extends BioUnit {
+
+    public Marine(String name, int hp) {
+        super(name, hp);
+    }
+}
---
+++ src/intermediate2/class1/generic/test/ex3/unit/Shuttle.java
@@ -0,0 +1,18 @@
+package intermediate2.class1.generic.test.ex3.unit;
+
+public class Shuttle<T extends BioUnit> {
+    private T unit;
+
+    public void in(T t) {
+        unit = t;
+    }
+
+    public T out() {
+        return unit;
+    }
+
+
+    public void showInfo() {
+        System.out.println("이름: " + unit.getName() + ", HP: " + unit.getHp());
+    }
+}
