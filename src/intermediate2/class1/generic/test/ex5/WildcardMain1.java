ca179417a64ec200d344fa5939d7f4dca0ea5b60
jeonginh0 <stitch010913@gmail.com>
2026-06-15 16:35:55 +0900

    study: 와일드카드 전환 학습

---
+++ src/intermediate2/class1/generic/test/ex5/WildcardMain1.java
@@ -0,0 +1,26 @@
+package intermediate2.class1.generic.test.ex5;
+
+import intermediate2.class1.generic.animal.Animal;
+import intermediate2.class1.generic.animal.Cat;
+import intermediate2.class1.generic.animal.Dog;
+
+public class WildcardMain1 {
+
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        dogBox.set(new Dog("멍멍이", 100));
+
+        WildcardEx.printGenericV1(dogBox);
+        WildcardEx.printWildcard(dogBox);
+
+        WildcardEx.printGenericV2(dogBox);
+        WildcardEx.printWildcardV2(dogBox);
+
+        Dog dog = WildcardEx.printAndReturnGeneric(dogBox);
+        Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
+    }
+
+}
