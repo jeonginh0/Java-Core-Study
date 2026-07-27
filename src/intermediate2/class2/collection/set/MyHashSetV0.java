9cb2061614bb05c6133a9d977e467abf245c2728
jeonginh0 <stitch010913@gmail.com>
2026-07-27 10:36:26 +0900

    study: 해시 학습

---
+++ src/intermediate2/class2/collection/set/MyHashSetV0.java
@@ -0,0 +1,38 @@
+package intermediate2.class2.collection.set;
+
+import java.util.Arrays;
+
+public class MyHashSetV0 {
+
+    private int[] elementData = new int[10];
+    private int size = 0;
+
+    // O(n) contains 때문에 다 뒤짐
+    public boolean add(int value) {
+        if (contains(value)) {
+            return false;
+        }
+
+        elementData[size] = value;
+        size++;
+        return true;
+    }
+
+    // O(n) 다 뒤지니까
+    public boolean contains(int value) {
+        for (int data : elementData) {
+            if (data == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV0{" +
+            "elementData=" + Arrays.toString(Arrays.copyOf(elementData, size)) +
+            ", size=" + size +
+            '}';
+    }
+}
