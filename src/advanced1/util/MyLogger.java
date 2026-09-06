a5682ea8418b8779caf92eab1ca0549527af8c71
jeonginh0 <stitch010913@gmail.com>
2026-09-06 14:55:02 +0900

    study: logger 구현

---
+++ src/advanced1/util/MyLogger.java
@@ -0,0 +1,16 @@
+package advanced1.util;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class MyLogger {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS "); //시, 분, 초, ms
+
+    public static void log(Object obj) {
+        String time = LocalTime.now().format(formatter);
+        System.out.printf("%s [%9s] %s\n", time, Thread.currentThread().getName(), obj);
+
+    }
+
+}
