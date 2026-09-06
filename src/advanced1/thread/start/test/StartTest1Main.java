1dd8718f43cebb18f63d97a113fea74f8ba79007
jeonginh0 <stitch010913@gmail.com>
2026-09-06 15:19:40 +0900

    study: 문제 풀이

---
+++ src/advanced1/thread/start/test/StartTest1Main.java
@@ -0,0 +1,25 @@
+package advanced1.thread.start.test;
+
+import static advanced1.util.MyLogger.*;
+
+public class StartTest1Main {
+
+    public static void main(String[] args) {
+        CounterThread thread = new CounterThread();
+        thread.start();
+    }
+
+    static class CounterThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                log("value: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
